edef7906652f3b56dfb070aa4609e4b965f8e728
Jannis R <mail@jannisr.de>
2017-11-24 02:02:47 +0100

    code style :shirt:

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const rad = (deg) => deg*Math.PI/180
+const rad = (deg) => deg * Math.PI / 180
 const sin = (deg) => Math.sin(rad(deg))
 const cos = (deg) => Math.cos(rad(deg))
 const tan = (deg) => Math.tan(rad(deg))
-const deg = (rad) => rad*180/Math.PI
+const deg = (rad) => rad * 180 / Math.PI
 
 const defaults = {
 	meridian: 0,
@@ -13,8 +13,15 @@ const defaults = {
 }
 
 const check = (point) => {
-	if(point.x!==undefined&&point.x>=0&&point.x<=1&&point.y!==undefined&&point.lon===undefined&&point.lat===undefined) return {x: +point.x, y: +point.y, wgs: false}
-	if(point.lon!==undefined&&point.lat!==undefined&&point.x===undefined&&point.y===undefined) return {lon: +point.lon, lat: +point.lat, wgs: true}
+	if (point.x !== undefined && point.x >= 0 && point.x <= 1 &&
+		point.y !== undefined &&
+		point.lon === undefined && point.lat === undefined) {
+		return {x: +point.x, y: +point.y, wgs: false}
+	}
+	if (point.lon !== undefined && point.lat !== undefined &&
+		point.x === undefined && point.y === undefined) {
+		return {lon: +point.lon, lat: +point.lat, wgs: true}
+	}
 	throw new Error('Invalid input point.')
 }
 
@@ -24,8 +31,16 @@ const options = (opt) => {
 
 const addMeridian = (point, meridian) => {
 	point = check(point)
-	if(meridian!==0) return check({lon: ((point.lon+180+360-meridian)%360)-180, lat: point.lat})
+	if (meridian !== 0) return check({
+		lon: ((point.lon + 180 + 360 - meridian) % 360) - 180,
+		lat: point.lat
+	})
 	return point
 }
 
-module.exports = {rad, sin, cos, tan, deg, addMeridian, check, options}
+module.exports = {
+	rad, sin, cos, tan, deg,
+	options,
+	check,
+	addMeridian
+}
